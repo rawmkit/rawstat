978d04e95c23fa7e713b362349595a0a6c2032b8
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-06 03:00:56 +0200

    config.h: there is no nerd font

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 static const Block blocks[] = {
 	// Icon		Command							Update Interval		Update Signal
 	{" ",		"/usr/share/dwmblocks/batt.sh",				2,			0},
-	{"",		"/usr/share/dwmblocks/audiovol.sh",			2,			0},
-	{"\uf85a ",	"/usr/share/dwmblocks/cpumem.sh",			2,			0},
-	{"\uf7c9 ",	"/usr/share/dwmblocks/diskfree.sh",			15,			0},
-	{"\uf5ef ",	"date '+%b %d (%a) %H:%M'",				5,			0},
+	{"avol:",	"/usr/share/dwmblocks/audiovol.sh",			2,			0},
+	{"cpumem:",	"/usr/share/dwmblocks/cpumem.sh",			2,			0},
+	{"dskfree:",	"/usr/share/dwmblocks/diskfree.sh",			15,			0},
+	{"",		"date '+%b %d (%a) %H:%M'",				5,			0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
