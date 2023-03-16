defb18809ab8f5f358406b4c2c3b39d54816acf4
Alexandr Savca <alexandr.savca89@gmail.com>
2023-03-16 12:55:37 +0200

    c89 comments

--- config.def.h
+++ config.def.h
@@ -1,17 +1,23 @@
-// dwmblocks customization.
-// See LICENSE file for copyright and license details.
+/* dwmblocks customization.
+ * See LICENSE file for copyright and license details. */
 
-// Modify this file to change what commands output to your statusbar,
-// and recompile using the make command.
+/*
+ * Modify this file to change what commands output to your statusbar,
+ * and recompile using the make command.
+ */
 static const Block blocks[] = {
-// Icon   Command                      Update Interval   Update Signal
-  {"",    "date '+%b %d (%a) %H:%M'",  5,                0},
+  /*
+   * Icon  Command                      Update Interval  Update Signal
+   */
+    {"",   "date '+%b %d (%a) %H:%M'",  5,               0},
 };
 
-// Sets delimiter between status commands.
-// NULL character ('\0') means no delimiter.
+/*
+ * Sets delimiter between status commands.
+ * NULL character ('\0') means no delimiter.
+ */
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
 
-// vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
-// End of file.
+/* vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
+ * End of file. */
