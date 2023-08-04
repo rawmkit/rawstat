6b3dc3ad45b9cc288142b741624330eb521fde3c
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-04 06:24:58 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,36 +1,36 @@
 # project metadata
-NAME = dwmblocks
-VERSION = 0.2z1
-DIST = ${NAME}-${VERSION}
+NAME      = dwmblocks
+VERSION   = 0.2z1
+DIST      = ${NAME}-${VERSION}
 
 # paths
 PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # X support (uncomment to disable X11)
-#NO_X  = -DNO_X
+#NO_X     = -DNO_X
 
 # DragonFlyBSD, FreeBSD
-#X11INC = /usr/local/include
-#X11LIB = /usr/local/lib
+#X11INC   = /usr/local/include
+#X11LIB   = /usr/local/lib
 
 # NetBSD, OpenBSD
-#X11INC = /usr/X11R6/include
-#X11LIB = /usr/X11R6/lib
+#X11INC   = /usr/X11R6/include
+#X11LIB   = /usr/X11R6/lib
 
 # Linux
-X11INC = /usr/include
-X11LIB = /usr/lib
+X11INC    = /usr/include
+X11LIB    = /usr/lib
 
 # includes and libs
-INCS = -I${X11INC}
-LIBS = -L${X11LIB} -lX11
+INCS      = -I${X11INC}
+LIBS      = -L${X11LIB} -lX11
 
 # flags
-CPPFLAGS = ${NO_X} ${INCS}
-CFLAGS   = -pedantic -Wall -Wextra -Wno-deprecated-declarations
-LDFLAGS  = ${LIBS}
+CPPFLAGS  = ${NO_X} ${INCS}
+CFLAGS    = -pedantic -Wall -Wextra -Wno-deprecated-declarations
+LDFLAGS   = ${LIBS}
 
 # compiler and linker
-CC = cc
-LD = ${CC}
+CC        = cc
+LD        = ${CC}
